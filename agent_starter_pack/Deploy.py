0aaf1bf035a72155c927974357ffe5e9f5b23d11
Samjad Azeez <samjad.azeez@walmart.com>
2025-10-21 17:09:51 -0500

    new file:   Deploy.sh 	new file:   agent_starter_pack/Deploy.py 	new file:   agent_starter_pack/agents/adk_base/app/requirements.txt

---
+++ agent_starter_pack/Deploy.py
@@ -0,0 +1,6 @@
+adk deploy agent_engine \
+  --project=jwmt-ade-agentspace \
+  --region=us-central1 \
+  --staging_bucket=gs://apex-agentengine-staging \
+  --display_name=BQToolsOAUTHAgentTEST \
+  ./bq_agent_app
