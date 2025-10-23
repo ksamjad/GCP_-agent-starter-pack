808df69dbee27687cc75166da28e1dccc28eff87
Samjad Azeez <samjad.azeez@walmart.com>
2025-10-23 09:31:31 -0500

    confirm

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,7 @@
-adk deploy agent_engine 
+adk deploy agent_engine `
   --project=wmt-ade-agentspace-dev `
   --region=us-central1 `
   --staging_bucket=gs://apex-agentengine-staging `
   --display_name=BQToolsOAUTHAgentTEST1 `
+  --temp_folder=D:\adk_temp `
   ./bq_agent_app
